d12ee692f328032fc4279adf902fa815c22ea003
Florian LAINE <florian.laine@epita.fr>
2023-02-07 23:33:29 +0100

    Working SDL window

--- dev/src/image_renderer.c
+++ dev/src/image_renderer.c
@@ -18,10 +18,9 @@ void degrade(SDL_Texture* texture, SDL_Renderer *renderer, int width, int height
     size_t i;
     int pitch;
     SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch);
-    printf("Lock\n");
     format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA8888);
     for(i = 0; i < width*height; i++)
-        pixels[i] = SDL_MapRGBA(format, i%255, 0, 0, 255);
+        pixels[i] = SDL_MapRGBA(format, i%width*(i/width)%255, 0, 0, 255);
     SDL_UnlockTexture(texture);
     SDL_FreeFormat(format);
 }
@@ -40,18 +39,9 @@ int launch(app_params* params){
     SDL_Texture* texture = SDL_CreateTexture(params->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, params->width, params->height);
     while (!quit)
     {
-        if(SDL_PollEvent(&event))
-        {
-            if(event.type == SDL_QUIT)
-            {
-                quit = 1;
-                break;
-            }
-        }
         SDL_RenderClear(params->renderer);
         degrade(texture, params->renderer, params->width, params->height); 
         SDL_RenderCopy(params->renderer, texture, &rect, &rect); 
-        free(texture);
         SDL_RenderPresent(params->renderer);
         fps++;
         if(last != time(NULL)){
@@ -59,10 +49,19 @@ int launch(app_params* params){
             fps = 0;
             last = time(NULL);
         }
+        if(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                free(texture);
+                //SDL_DestroyRenderer(params->renderer); 
+                SDL_DestroyWindow(params->window);
+                SDL_Quit();
+                return 0;
+            }
+        }
     }
-    SDL_DestroyRenderer(params->renderer); 
-    SDL_DestroyWindow(params->window);
-    SDL_Quit();
     return 0;
 }
+
 
