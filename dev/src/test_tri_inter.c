e9fba13bf4fa69b23edf773b4422798345c6430b
tim pearson <timothy-edward.pearson@epita.fr>
2023-02-08 23:11:57 +0100

    testing in test_tri_inter.c

---
+++ dev/src/test_tri_inter.c
@@ -0,0 +1,33 @@
+#include "image_renderer.c"
+#include "utils.c"
+#include <stdio.h>
+
+int main(){
+    mesh * m = init_mesh(3, 1, 0);
+    add_vertex(m, 0, -10, 0, 0);
+    add_vertex(m, 0, 20, 0, 0);
+    add_vertex(m, 0, 0, 20, 0);
+    add_tri(m, 0, 0,1, 2);
+    m->triangles[0]->normal = *init_point(0, 0, 0, 1);
+
+
+    ray * r = init_ray(0, *init_point(0, 0, 10, 10),
+                       *init_point(0, 0, -10, -20));
+
+
+    //ray_intersect(r, m->vertexes[m->triangles[0]->vert[0]], &m->triangles[0]->normal, m);
+
+    hit_triangle(r, m, 0);
+    printf("contact  =    %f %f %f\n", r->contact.x, r->contact.y, r->contact.z);
+    if(r->hit ==1 ){
+        printf("hit\n");
+    }
+    else{
+        printf("miss\n");
+    }
+    // printf("ray pos = %f, %f, %f\n", r->pos.x, r->pos.y, r->pos.z);
+
+
+
+    return 0;
+}
--- dev/src/utils.c
+++ dev/src/utils.c
@@ -260,45 +260,58 @@ float area_triangle(point * a, point * b, point * c)
 
 }
 
+
+void ray_intersect(ray * ray, point * vertex, point * normal)
+{
+
+    float d = dot(normal, vertex);
+    float n_p = dot(normal, &ray->pos);
+    float n_rd = dot(normal, &ray->dir);
+    printf("d = %f\n" , d);
+    printf("n_p = %f\n" , n_p);
+    printf("n_rd = %f\n" , n_rd);
+    float t = (d - n_p)/n_rd;
+    printf("t: %f\n", t);
+    if(t > 0)
+    {
+
+        point  p = {0, ray->pos.x + t*ray->dir.x, ray->pos.y + t*ray->dir.y, ray->pos.z + t*ray->dir.z};
+
+        ray->contact = p;
+        ray->hit = -1;
+
+    }
+    else
+    {
+        ray->hit = 0;
+    }
+
+
+}
+
+
 void hit_triangle(ray * ray, mesh * mesh, size_t index)
 {
 
     point * a = mesh->vertexes[mesh->triangles[index]->vert[0]];
     point * b = mesh->vertexes[mesh->triangles[index]->vert[1]];
     point * c = mesh->vertexes[mesh->triangles[index]->vert[2]];
+    minus(a,c);
+    ray_intersect(ray, a, &mesh->triangles[index]->normal);
+    if (ray->hit == -1) {
 
 
-    point * p = &ray->contact;
-    if (area_triangle(a, b, c) == area_triangle(p, a, b) + area_triangle(p, b, c) + area_triangle(p, c, a))
-    {
-        ray->hit = 1;
+        point *p = &ray->contact;
+        if (area_triangle(a, b, c) == area_triangle(p, a, b) + area_triangle(p, b, c) + area_triangle(p, c, a)) {
+            ray->hit = 1;
+            printf("yess\n");
 
+        }
     }
 
 }
 
 
-
-void ray_intersect(ray * ray, point * vertex, point * normal, mesh * m)
-{
-
-    float d = dot(normal, vertex);
-    float n_p = dot(normal, &ray->pos);
-    float n_rd = dot(normal, &ray->dir);
-
-    float t = (d - n_p)/n_rd;
-    printf("t: %f\n", t);
-    if(t > 0)
-    {
-
-
-        point  p = {ray->pos.x + t*ray->dir.x, ray->pos.y + t*ray->dir.y, ray->pos.z + t*ray->dir.z};
-        ray->contact = p;
-
-    }
-
-
-}
 
 
 
