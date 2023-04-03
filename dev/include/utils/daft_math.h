18140082c788194baa47fab6bb25aa78ae1edc87
David <david.wu@epita.fr>
2023-04-03 19:44:30 +0200

    add function distance in math daft

--- dev/include/utils/daft_math.h
+++ dev/include/utils/daft_math.h
@@ -11,6 +11,7 @@
 #define scale(a, b, r) r[0] = a[0] * b; r[1] = a[1] * b; r[2] = a[2] * b;
 #define normSquared(a) a[0] * a[0] + a[1] * a[1] + a[2] * a[2];
 #define norm(a) sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+#define distance(a,b)sqrt(pow(b[0]-a[0],2)+ pow(b[1]-a[1],2)+pow(b[2]-a[2],2))
 #define projectOn(a, b, r) scale(b, dotProduct(a, b) / dotProduct(b, b), r);
 
 void normalize(float* p, float* r);
