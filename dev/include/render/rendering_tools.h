29ec20a3c294aa281bf1c6a5eca71e0600d0ca2f
Frayz3n <florian.laine@epita.fr>
2023-05-24 02:07:06 +0200

    Set up sample scene

--- dev/include/render/rendering_tools.h
+++ dev/include/render/rendering_tools.h
@@ -6,5 +6,5 @@
 
 rendering_params* init_rendering_params(world *w, int width, int height, camera* cam, float screen_scale, Uint32* pixels);
 void free_rendering_params(rendering_params* rdp);
-raycast_param* init_raycast_param(ray* r, world* w, int reflection, int shadow, int skybox);
+raycast_param* init_raycast_param(ray* r, world* w, int reflection, int shadow, int shadow_mask, int skybox);
 #endif
