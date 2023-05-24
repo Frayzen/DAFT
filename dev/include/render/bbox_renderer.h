6262d844710a95a9307c67bdefd52a68365b502b
Frayz3n <florian.laine@epita.fr>
2023-05-24 02:27:16 +0200

    Removed dev

--- dev/include/render/bbox_renderer.h
+++
@@ -1,9 +0,0 @@
-#include "../architecture/bbox.h"
-#include "../architecture/ray.h"
-#include "./triangle_renderer.h"
-#include <math.h>
-#ifndef BBOX_RENDERER_H
-#define BBOX_RENDERER_H
-
-int bbox_render(bbox * b, ray * r);
-#endif
