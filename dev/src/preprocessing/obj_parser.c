e2391b56a429f7b274ece8905be36b5d34fb5b1b
flo <pub2flo@gmail.com>
2023-08-13 02:11:41 +0200

    Obj Parser

--- dev/src/preprocessing/obj_parser.c
+++ dev/src/preprocessing/obj_parser.c
@@ -1,25 +1,24 @@
 #include "../../include/preprocessing/obj_parser.h"
 
-void rotate(float* p, float rotX, float rotY, float rotZ){
-    float tmp[3];
-    tmp[0] = p[0];
-    tmp[1] = p[1];
-    tmp[2] = p[2];
+void rotate(float3* r, float rotX, float rotY, float rotZ){
+    float3 p = *r;
+    float3 tmp;
+    copy(p, tmp);
     if(rotX != 0){
-        p[1] = tmp[1] * cos(rotX) - tmp[2] * sin(rotX);
-        p[2] = tmp[1] * sin(rotX) + tmp[2] * cos(rotX);
+        p.y = tmp.y * cos(rotX) - tmp.z * sin(rotX);
+        p.z = tmp.y * sin(rotX) + tmp.z * cos(rotX);
     }
     if(rotY != 0){
-        p[0] = tmp[0] * cos(rotY) + tmp[2] * sin(rotY);
-        p[2] = -tmp[0] * sin(rotY) + tmp[2] * cos(rotY);
+        p.x = tmp.x * cos(rotY) + tmp.z * sin(rotY);
+        p.z = -tmp.x * sin(rotY) + tmp.z * cos(rotY);
     }
     if(rotZ != 0){
-        p[0] = tmp[0] * cos(rotZ) - tmp[1] * sin(rotZ);
-        p[1] = tmp[0] * sin(rotZ) + tmp[1] * cos(rotZ);
+        p.x = tmp.x * cos(rotZ) - tmp.y * sin(rotZ);
+        p.y = tmp.x * sin(rotZ) + tmp.y * cos(rotZ);
     }
 }
 
-int load_object(char *path, world *w, float scale, float pos[3], float rot[3], char *rel_path)
+int load_object(char *path, world *w, float scale, float3 pos, float3 rot, char *rel_path)
 {
     FILE *file;
     file = fopen(path, "r");
@@ -82,14 +81,14 @@ int load_object(char *path, world *w, float scale, float pos[3], float rot[3], c
     mesh *new_mesh = build_mesh(vert, tri, text_vert, norm_vert);
     fseek(file, 0, SEEK_SET);
 
-    float v[3];
-    float vn[3];
-    float vt[2];
+    float3 v;
+    float3 vn;
+    float2 vt;
     material *curr = NULL;
 
-    float rotX = rot[0] * M_PI / 180;
-    float rotY = rot[1] * M_PI / 180;
-    float rotZ = rot[2] * M_PI / 180;
+    float rotX = rot.x * M_PI / 180;
+    float rotY = rot.y * M_PI / 180;
+    float rotZ = rot.z * M_PI / 180;
 
     while (fgets(line, sizeof(line), file) != NULL)
     {
@@ -97,20 +96,20 @@ int load_object(char *path, world *w, float scale, float pos[3], float rot[3], c
         {
             if (line[1] == ' ')
             {
-                sscanf(line, "v %f %f %f", &v[0], &v[1], &v[2]);
-                rotate(v, rotX, rotY, rotZ);
+                sscanf(line, "v %f %f %f", &v.x, &v.y, &v.z);
+                rotate(&v, rotX, rotY, rotZ);
                 scale(v, scale, v);
                 add(v, pos, v);
                 add_v(new_mesh, v);
             }
             if (line[1] == 't')
             {
-                sscanf(line, "vt %f %f", &vt[0], &vt[1]);
+                sscanf(line, "vt %f %f", &vt.x, &vt.y);
                 add_vt(new_mesh, vt);
             }
             if (line[1] == 'n')
             {
-                sscanf(line, "vn %f %f %f", &vn[0], &vn[1], &vn[2]);
+                sscanf(line, "vn %f %f %f", &vn.x, &vn.y, &vn.z);
                 add_vn(new_mesh, vn);
             }
         }
@@ -166,40 +165,40 @@ int load_object(char *path, world *w, float scale, float pos[3], float rot[3], c
                 i++;
             }
 
-            int v[3];
-            int vt[3];
-            int vn[3];
+            int3 v;
+            int3 vt;
+            int3 vn;
             for (int j = 0; j < nb_v - 2; j++)
             {
-                v[0] = vs[0][0] - 1;
-                if (v[0] < 0)
-                    v[0] += vert;
-                v[1] = vs[0][j + 1] - 1;
-                if (v[1] < 0)
-                    v[1] += vert;
-                v[2] = vs[0][j + 2] - 1;
-                if (v[2] < 0)
-                    v[2] += vert;
+                v.p1 = vs[0][0] - 1;
+                if (v.p1 < 0)
+                    v.p1 += vert;
+                v.p2 = vs[0][j + 1] - 1;
+                if (v.p2 < 0)
+                    v.p2 += vert;
+                v.p3 = vs[0][j + 2] - 1;
+                if (v.p3 < 0)
+                    v.p3 += vert;
 
-                vt[0] = vs[1][0] - 1;
-                if (vt[0] < 0)
-                    vt[0] += text_vert;
-                vt[1] = vs[1][j + 1] - 1;
-                if (vt[1] < 0)
-                    vt[1] += text_vert;
-                vt[2] = vs[1][j + 2] - 1;
-                if (vt[2] < 0)
-                    vt[2] += text_vert;
+                vt.p1 = vs[1][0] - 1;
+                if (vt.p1 < 0)
+                    vt.p1 += text_vert;
+                vt.p2 = vs[1][j + 1] - 1;
+                if (vt.p2 < 0)
+                    vt.p2 += text_vert;
+                vt.p3 = vs[1][j + 2] - 1;
+                if (vt.p3 < 0)
+                    vt.p3 += text_vert;
 
-                vn[0] = vs[2][0] - 1;
-                if (vn[0] < 0)
-                    vn[0] += norm_vert;
-                vn[1] = vs[2][j + 1] - 1;
-                if (vn[1] < 0)
-                    vn[1] += norm_vert;
-                vn[2] = vs[2][j + 2] - 1;
-                if (vn[2] < 0)
-                    vn[2] += norm_vert;
+                vn.p1 = vs[2][0] - 1;
+                if (vn.p1 < 0)
+                    vn.p1 += norm_vert;
+                vn.p2 = vs[2][j + 1] - 1;
+                if (vn.p2 < 0)
+                    vn.p2 += norm_vert;
+                vn.p3 = vs[2][j + 2] - 1;
+                if (vn.p3 < 0)
+                    vn.p3 += norm_vert;
                 add_tri(new_mesh, v, vt, vn, curr);
             }
             for (int i = 0; i < 3; i++)
@@ -227,17 +226,17 @@ int load_object(char *path, world *w, float scale, float pos[3], float rot[3], c
         //load_texture(mats, "./assets/textures/leather_white_diff_4k.jpg");
     }
     */
-    printf("Bbox of the object created: from %f %f %f to %f %f %f\n", new_mesh->box->min[0], new_mesh->box->min[1], new_mesh->box->min[2], new_mesh->box->max[0], new_mesh->box->max[1], new_mesh->box->max[2]);
-    printf("Mesh loaded : %s (%d vertices, %d triangles)\n", path, new_mesh->nb_vertices, new_mesh->nb_triangles);
+    printf("Bbox of the object created: from %f %f %f to %f %f %f\n", new_mesh->b_min[0].x, new_mesh->b_min[0].y, new_mesh->b_min[0].z, new_mesh->b_max[0].x, new_mesh->b_max[0].y, new_mesh->b_max[0].z);
+    printf("Mesh loaded : %s (%d vertices, %d triangles)\n", path, new_mesh->vs_size, new_mesh->tri_size);
     new_mesh->mats = mats;
-    new_mesh->nb_mat = nb_mat;
+    new_mesh->mat_size = nb_mat;
     // print all the mats
-    for (int i = 0; i < new_mesh->nb_mat; i++)
+    for (int i = 0; i < new_mesh->mat_size; i++)
     {
         printf("Material %d\n", i);
-        printf("ka is %f %f %f\n", new_mesh->mats[i].ambient[0], new_mesh->mats[i].ambient[1], new_mesh->mats[i].ambient[2]);
-        printf("kd is %f %f %f\n", new_mesh->mats[i].diffuse[0], new_mesh->mats[i].diffuse[1], new_mesh->mats[i].diffuse[2]);
-        printf("ks is %f %f %f\n", new_mesh->mats[i].specular[0], new_mesh->mats[i].specular[1], new_mesh->mats[i].specular[2]);
+        printf("ka is %f %f %f\n", new_mesh->mats[i].ambient.x, new_mesh->mats[i].ambient.y, new_mesh->mats[i].ambient.z);
+        printf("kd is %f %f %f\n", new_mesh->mats[i].diffuse.x, new_mesh->mats[i].diffuse.y, new_mesh->mats[i].diffuse.z);
+        printf("ks is %f %f %f\n", new_mesh->mats[i].specular.x, new_mesh->mats[i].specular.y, new_mesh->mats[i].specular.z);
     }
     add_mesh(w, new_mesh);
     fclose(file);
